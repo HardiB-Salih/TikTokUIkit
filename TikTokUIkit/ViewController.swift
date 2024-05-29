f7212ef64ec9e3207f43c661629e65f4cefafa24
HardiB.Salih <hardib.salih@192.168.1.47>
2024-05-29 12:44:19 +0300

    setting up the project

---
+++ TikTokUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokUIkit
+//
+//  Created by HardiB.Salih on 5/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
