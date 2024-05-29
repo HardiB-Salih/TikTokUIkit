8dfd4fef5f879ec425e7cbf9e7ceeb32fc130c9e
HardiB.Salih <hardib.salih@192.168.1.47>
2024-05-29 14:23:10 +0300

    tabbarController been set up for the project.

---
+++ TikTokUIkit/Controllers/Core/TabBarVC.swift
@@ -0,0 +1,48 @@
+//
+//  TabBarVC.swift
+//  TikTokUIkit
+//
+//  Created by HardiB.Salih on 5/29/24.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupController()
+    }
+    
+    
+    
+    
+    
+    
+    private func setupController() {
+        let home = HomeVC()
+        let explore = ExploreVC()
+        let camera = CameraVC()
+        let notification = NotificationVC()
+        let profile = ProfileVC()
+        
+        home.title = "Home"
+        explore.title = "Explore"
+        camera.title = "Camera"
+        notification.title = "Notification"
+        profile.title = "Profile"
+        
+        let nav1 = UINavigationController(rootViewController: home)
+        let nav2 = UINavigationController(rootViewController: explore)
+        let nav3 = UINavigationController(rootViewController: notification)
+        let nav4 = UINavigationController(rootViewController: profile)
+        
+        nav1.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "binoculars") , tag: 2)
+        camera.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "camera") , tag: 3)
+        nav3.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bell.badge") , tag: 4)
+        nav4.tabBarItem = UITabBarItem(title: nil, image:UIImage(systemName: "person") , tag: 5)
+
+        setViewControllers([nav1, nav2, camera, nav3, nav4], animated: false)
+    }
+}
--- TikTokUIkit/Resorces/SceneDelegate.swift
+++ TikTokUIkit/Resorces/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = HomeVC()
+        window.rootViewController = TabBarVC()
         self.window = window
         self.window?.makeKeyAndVisible()
     }
